9d67456a4f7ca10a2b3ede16cb6970e75d3b4f6c
César Subtil <cesar.subtil.2000@gmail.com>
2022-06-29 17:08:27 +0100

    corrected merge mistake

--- DJDM/Assets/Scripts/ObjectPoolingManager.cs
+++ DJDM/Assets/Scripts/ObjectPoolingManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class ObjectPoolingManager : MonoBehaviour {
+
     public static ObjectPoolingManager Instance { get; private set; }
 
     [SerializeField]
