a7fbff67a96f49e388d5e359d8d4fb786ce1397b
Dev-Alex10 <kitkatpt1234@gmail.com>
2022-07-03 18:06:17 +0100

    Added boss shoot, one chest, updated lvl

--- DJDM/Assets/Scripts/UpgradesController.cs
+++ DJDM/Assets/Scripts/UpgradesController.cs
@@ -2,28 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UpgradesController : MonoBehaviour
-{
+public class UpgradesController : MonoBehaviour {
     private Animator myAnimator = null;
     [SerializeField]
     private string type = null;
 
+    private bool flag = false;
 
-    private void Awake()
-    {
+    private void Awake() {
         myAnimator = GetComponent<Animator>();
     }
 
-    public void OpenChest()
-    {
+    public void OpenChest() {
         Debug.Log(GameManager.Instance.DamageMultiplier());
-        if (!myAnimator.GetBool("open"))
-        {
+        if (!myAnimator.GetBool("open")) {
             myAnimator.SetBool("open", true);
 
             GameManager.Instance.Upgrade(type);
 
         }
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player") && !flag) {
+            myAnimator.SetTrigger("Open");
+            GameManager.Instance.Upgrade(type);
+            flag = true;
+        }
     }
 }
